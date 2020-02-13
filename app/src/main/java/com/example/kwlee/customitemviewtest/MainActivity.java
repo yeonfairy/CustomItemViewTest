c369f1eb234be7ab1c4ada9bb9fa8f6cd01092b3
yeonfairy <niceboop@naver.com>
2020-02-13 17:44:25 +0900

    second commit: 메뉴바 추가

--- app/src/main/java/com/example/kwlee/customitemviewtest/MainActivity.java
+++ app/src/main/java/com/example/kwlee/customitemviewtest/MainActivity.java
@@ -2,25 +2,18 @@ package com.example.kwlee.customitemviewtest;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TabHost;
-import android.widget.TabWidget;
 import android.widget.Toast;
 
-import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -201,5 +194,12 @@ public class MainActivity extends AppCompatActivity {
             alBuilder.setTitle("프로그램 종료");
             alBuilder.show(); // AlertDialog.Bulider로 만든 AlertDialog를 보여준다.
         }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu2) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu2, menu2);
+        return true;
+    }
     }
 
